a4632359ae6c4a1b688bd1afaafe5c51e75b0777
cwktomas223 <tomas.brejl@gmail.com>
2019-07-01 14:22:49 -0700

    add a conditional exercise

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ def main():
 
 if __name__=="__main__":
     main()
+
+name = input("what is your name?")
+print("hello %s" %name)
+
+num = int(input("enter a number"))
+if num > 10:
+    print ("the number is greater than 10")
+elif number < 10:
+    print("the number is less than 10.")
+else:
+    print("you did not provide a number")
